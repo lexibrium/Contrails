696a9aeb71c496aed6b4052215cb20e574f8ed2c
lexibrium <44617235+lexibrium@users.noreply.github.com>
2024-09-05 10:59:44 -0700

    Update configs.js
    tolkien configs

--- cloudflare-worker/configs.js
+++ cloudflare-worker/configs.js
@@ -1,68 +1,17 @@
 export const CONFIGS = {
-  "emotional-suppo": {
-    "recordName": "emotional-suppo",
-    "displayName": "Emotional Support Pets",
-    "description": "Cute animals feed",
+  "tolkien": {
+    "recordName": "tolkien",
+    "displayName": "Tolkien",
+    "description": "Find Tolkien related posts on Bluesky",
     "searchTerms": [
-      "at://did:plc:ozppa2bsq6bdnajyweoir2i2/app.bsky.feed.post/3jxju2wwap22e",
-      "cats",
-      "dogs",
-      "penguins",
-      "red pandas",
-      "quokkas"
+      "tolkien",
+      "Middle-earth",
+      '"Lord of the Rings"',
+      "Hobbit",
+      '"Rings of Power"',
+      "at://did:plc:tjqjxamiekx4ihexr57ezs36/app.bsky.feed.post/3jyu2fzmbim2t"
     ],
-    "denyList": [],
-    "safeMode": true,
-    "avatar": "avatar.png",
+    "avatar": "tcg-red-avatar.jpg",
     "isEnabled": true
-  },
-  "bluesky-app": {
-    "recordName": "bluesky-app",
-    "isEnabled": false,
-    "displayName": "Bluesky App",
-    "description": "Updates about the app",
-    "searchTerms": [
-      "at://did:plc:z72i7hdynmk6r22z27h6tvur",
-      "at://did:plc:ewvi7nxzyoun6zhxrhs64oiz"
-    ],
-    "safeMode": true,
-    "avatar": "configs/avatar2.png"
-  },
-  "science-emojis": {
-    "recordName": "science-emojis",
-    "isEnabled": false,
-    "displayName": "Science Emojis",
-    "description": "Posts with \ud83e\uddea\ud83e\udd7c\ud83d\udd2d",
-    "searchTerms": [
-      "\ud83e\uddea",
-      "\ud83e\udd7c",
-      "\ud83d\udd2d"
-    ],
-    "safeMode": true,
-    "avatar": "configs/avatar2.png"
-  },
-  "gaming-emojis": {
-    "recordName": "gaming-emojis",
-    "isEnabled": false,
-    "displayName": "Gaming Emojis",
-    "description": "Posts with \ud83d\udc7e\ud83c\udfae\ud83d\udd79\ufe0f",
-    "searchTerms": [
-      "\ud83d\udc7e",
-      "\ud83c\udfae",
-      "\ud83d\udd79\ufe0f"
-    ],
-    "safeMode": true,
-    "avatar": "configs/avatar2.png"
-  },
-  "basketball-emoj": {
-    "recordName": "basketball-emoj",
-    "isEnabled": false,
-    "displayName": "Basketball Emojis",
-    "description": "Posts with \ud83c\udfc0",
-    "searchTerms": [
-      "\ud83c\udfc0"
-    ],
-    "safeMode": true,
-    "avatar": "configs/avatar2.png"
   }
 }
